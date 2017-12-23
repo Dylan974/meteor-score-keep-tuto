631081b0b6901c81034f670ec9e90b7bd81a1b01
unknown <mugidylan@gmail.com>
2017-12-23 11:37:40 +0400

    Score App with component

--- client/main.js
+++ client/main.js
@@ -4,59 +4,19 @@ import { Meteor } from 'meteor/meteor';
 import { Tracker } from 'meteor/tracker';
 
 import { Players } from '../imports/api/players';
-
-const renderPlayer = (playersList) => {
-    return playersList.map((player) => {
-        return (
-            <p key={player._id}>
-                {player.name} has {player.score} point(s).
-                <button onClick={() => {
-                    Players.update({_id : player._id}, {
-                        $inc:{score: 1}
-                    });
-                }}>+1</button>
-                <button onClick={() => {
-                    Players.update({_id : player._id}, {
-                        $inc:{score: -1}
-                    });
-                }}>-1</button>
-                <button onClick={() => Players.remove({_id : player._id})}>X</button>
-            </p>
-        );
-    });
-};
-
-const handleSubmit = (e) => {
-    let playerName = e.target.playerName.value;
-    e.preventDefault();
-
-    if(playerName){
-        e.target.playerName.value = '';
-        Players.insert({
-            name: playerName,
-            score: 0
-        });
-    }
-};
+import App from '../imports/ui/App';
 
 Meteor.startup(() => {
     Tracker.autorun(() => {
-        let players = Players.find().fetch();
-
+        let players = Players.find({}, {
+            sort: {
+                score: -1,
+                name: 1
+            }
+        }).fetch();
         let title = 'Score Keep';
-        let name = 'Dylan';
-        let jsx = (
-            <div>
-                <h1>{title}</h1>
-                <p>Hello {name}!</p>
-                {renderPlayer(players)}
-                <form onSubmit={handleSubmit}>
-                    <input type="text" name="playerName" placeholder="Player name"/>
-                    <button>Add player</button>
-                </form>
-            </div>
-        );
-        ReactDom.render(jsx, document.getElementById('app'));    
+        let subtitle = 'Created by Dylan';
+        ReactDom.render(<App title={title} players={players}/>, document.getElementById('app'));    
     });
     
 });
