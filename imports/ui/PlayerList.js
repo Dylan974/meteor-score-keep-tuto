99a1e9e333dfd0d58bdc26b1722d5b27f872f23b
unknown <mugidylan@gmail.com>
2017-12-23 14:43:37 +0400

    Adding some scss with BEM

--- imports/ui/PlayerList.js
+++ imports/ui/PlayerList.js
@@ -6,10 +6,14 @@ import Player from './Player';
 export default class PlayerList extends React.Component {
     renderPlayers(){
         if(this.props.players.length === 0){
-            return <p>Add your first player to get strated</p>
+            return (
+                <div className="item">
+                    <p className="item__message">Add your first player to get strated</p>
+                </div>
+            );
         } else {
             return this.props.players.map((player) => {
-                return <Player key={player._id} player={player}/>
+                return <Player key={player._id} player={player}/>;
             });
         }
     }
